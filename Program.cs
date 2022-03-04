d1d994cf4386686af926d9999dc4620bec1631dd
annagaetakirsten <anna.agkirsten@gmail.com>
2022-03-04 17:57:40 -0300

    tarefa realizada

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("mongagua");
+Console.WriteLine("praia grande");
+Console.WriteLine("itanhaem");
+Console.WriteLine("peruibe");
+Console.WriteLine("sao vicente");
